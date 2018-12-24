b02a9a4e7caf354a9e42e0118ee0acb9e4ef86f2
Trần Quốc Đạt <quocdat23894@gmail.com>
2018-12-24 03:52:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // write your code below!
 function happyHolidays() {
   return "Happy holidays!"
-}
+} 
 
 function happyHolidaysTo(name) {
-    return "Happy holidays, ${name}"
+    return "Happy holidays, `${name}`"
 }
